aed3bb01e7e83c1279155c4a4e1a704e4e683d4d
sjkang <sjkang@byyj.co.kr>
2024-06-19 18:16:23 +0900

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,11 +10,11 @@ export default function Header() {
 		<HeaderSearch />
 		
 		<nav className={styles.header_nav}>
-			<a href="javascript:;" className={styles.header_nav__active}><span>전체</span></a>
-			<a href="javascript:;">팔로잉</a>
-			<a href="javascript:;">우먼</a>
-			<a href="javascript:;">맨</a>
-			<a href="javascript:;">5월의 탑셀럽</a>
+			<a href={"#"} className={styles.header_nav__active}><span>전체</span></a>
+			<a href={"#"}>팔로잉</a>
+			<a href={"#"}>우먼</a>
+			<a href={"#"}>맨</a>
+			<a href={"#"}>5월의 탑셀럽</a>
 		</nav>
 		<div className={styles.header_favorite}>
 			<p>
